2938e955c83d59c87ea8942d79ee9dcd842d8d63
Yves Candau <ycandau@sfu.ca>
2021-08-19 01:50:59 +0000

    Migration: create new user table

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -11,7 +11,6 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-
     if @product.save
       redirect_to [:admin, :products], notice: 'Product created!'
     else
