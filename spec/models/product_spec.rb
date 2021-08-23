a903534790471bb9b36ed8357ebe0283d297c4ac
Yves Candau <ycandau@sfu.ca>
2021-08-23 23:19:04 +0000

    Add spec: product not created if quantity not present

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     
-    it 'should create a new product if all the fields are provided' do
+    it 'should create a new product if all the fields are present' do
       @category = Category.new(name: 'Test category')
       @product = Product.new(
         name: 'Man-bun tie',
@@ -14,7 +14,7 @@ RSpec.describe Product, type: :model do
       expect(@product.valid?).to be(true)
     end
 
-    it 'should not create a new product if the name is missing' do
+    it 'should not create a new product if the name is not present' do
       @category = Category.new(name: 'Test category')
       @product = Product.new(
         price_cents: 9999,
@@ -25,7 +25,7 @@ RSpec.describe Product, type: :model do
       expect(@product.errors[:name]).to match(["can't be blank"])
     end
 
-    it 'should not create a new product if the price is missing' do
+    it 'should not create a new product if the price is not present' do
       @category = Category.new(name: 'Test category')
       @product = Product.new(
         name: 'Man-bun tie',
@@ -34,6 +34,17 @@ RSpec.describe Product, type: :model do
       )
       expect(@product.valid?).to be(false)
       expect(@product.errors[:price_cents]).to match(["is not a number"])
+    end
+
+    it 'should not create a new product if the quantity is not present' do
+      @category = Category.new(name: 'Test category')
+      @product = Product.new(
+        name: 'Man-bun tie',
+        price_cents: 9999,
+        category: @category
+      )
+      expect(@product.valid?).to be(false)
+      expect(@product.errors[:quantity]).to match(["can't be blank"])
     end
 
   end
