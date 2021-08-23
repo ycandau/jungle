0b8fd9fa27a686a1dc1d49e8926d8a517714eb5e
Yves Candau <ycandau@sfu.ca>
2021-08-23 23:10:38 +0000

    Add spec: product not created when name not present

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe Product, type: :model do
       )
       expect(@product.valid?).to be(true)
     end
+
+    it 'should not create a new product if the name is missing' do
+      @category = Category.new(name: 'Test category')
+      @product = Product.new(
+        price_cents: 9999,
+        quantity: 123,
+        category: @category
+      )
+      expect(@product.valid?).to be(false)
+      expect(@product.errors[:name]).to match(["can't be blank"])
+    end
+    
   end
 end
