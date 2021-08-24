307459094a8691a4fb48081aaf5bd162d5c1250d
Yves Candau <ycandau@sfu.ca>
2021-08-24 01:36:30 +0000

    Change authentication method to work for edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
   
   def self.authenticate_with_credentials(email, password)
-    @user = User.find_by_email(email)
+    
+    @user = User.find_by_email(email.strip.downcase)
     if @user and @user.authenticate(password)
       @user
     else
