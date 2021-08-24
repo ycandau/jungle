f91c3b661e9afe1d23828f3adef7829c1cb353ce
Yves Candau <ycandau@sfu.ca>
2021-08-24 00:15:38 +0000

    Add active record validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
   
 end
