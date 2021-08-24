3ac7a86c407133a831c95aba644160e5b23ab960
Yves Candau <ycandau@sfu.ca>
2021-08-24 00:26:36 +0000

    Add spec: user not created if any field not present

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,9 +9,69 @@ RSpec.describe User, type: :model do
         last_name: 'Frum',
         email: 'ffrum@gmail.com',
         password: 'sequoia',
-        password_confirmation: 'sequoia',
+        password_confirmation: 'sequoia'
       )
       expect(@user.valid?).to be(true)
+    end
+
+    it 'should not create a new user if the first name is not present' do
+      @user = User.new(
+        first_name: nil,
+        last_name: 'Frum',
+        email: 'ffrum@gmail.com',
+        password: 'sequoia',
+        password_confirmation: 'sequoia'
+      )
+      expect(@user.valid?).to be(false)
+      expect(@user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it 'should not create a new user if the last name is not present' do
+      @user = User.new(
+        first_name: 'Fred',
+        last_name: nil,
+        email: 'ffrum@gmail.com',
+        password: 'sequoia',
+        password_confirmation: 'sequoia'
+      )
+      expect(@user.valid?).to be(false)
+      expect(@user.errors[:last_name]).to include("can't be blank")
+    end
+
+    it 'should not create a new user if the email is not present' do
+      @user = User.new(
+        first_name: 'Fred',
+        last_name: 'Frum',
+        email: nil,
+        password: 'sequoia',
+        password_confirmation: 'sequoia'
+      )
+      expect(@user.valid?).to be(false)
+      expect(@user.errors[:email]).to include("can't be blank")
+    end
+
+    it 'should not create a new user if the password is not present' do
+      @user = User.new(
+        first_name: 'Fred',
+        last_name: 'Frum',
+        email: 'ffrum@gmail.com',
+        password: nil,
+        password_confirmation: 'sequoia'
+      )
+      expect(@user.valid?).to be(false)
+      expect(@user.errors[:password]).to include("can't be blank")
+    end
+
+    it 'should not create a new user if the password confirmation is not present' do
+      @user = User.new(
+        first_name: 'Fred',
+        last_name: 'Frum',
+        email: 'ffrum@gmail.com',
+        password: 'sequoia',
+        password_confirmation: nil
+      )
+      expect(@user.valid?).to be(false)
+      expect(@user.errors[:password_confirmation]).to include("can't be blank")
     end
 
   end
